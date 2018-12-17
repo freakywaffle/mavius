3c5c294cc823ddf285786986e1a2a0b959a079b0
nodoi <seyeon1491@naver.com>
2018-12-17 10:10:57 +0900

    [세연1217 2차]

--- src/main/java/com/mavius/web/controller/admin/MemberListController.java
+++ src/main/java/com/mavius/web/controller/admin/MemberListController.java
@@ -12,8 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/admin/member/management")
-public class IndexController extends HttpServlet{
+@WebServlet("/admin/member/management/list")
+public class MemberListController extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
